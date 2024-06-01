ca2a10e74cbc4820f1e507d8cc5fcb003d6eccb0
Vedant Agrawal <96226581+vedantagrawal524@users.noreply.github.com>
2024-06-02 01:07:56 +0530

    web chat App bar added

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/widgets/contact_list.dart';
+import 'package:whatsapp/widgets/web_chat_appbar.dart';
 import 'package:whatsapp/widgets/web_profile_bar.dart';
 import 'package:whatsapp/widgets/web_search_bar.dart';
 
@@ -30,6 +31,11 @@ class WebScreenLayout extends StatelessWidget {
                 image: AssetImage('assets/backgroundImage.png'),
                 fit: BoxFit.cover,
               ),
+            ),
+            child: Column(
+              children: [
+                WebChatAppbar(),
+              ],
             ),
           )
         ],
